98bced293726e6ee4f3e7b613ac43561054da3ad
shivanshu <77038395+shivanshubisht@users.noreply.github.com>
2024-05-02 10:12:55 +0000

    bump clerk to v5.0.5 and update matchers

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,9 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 const isPublicRoute = createRouteMatcher([
+  '/account/create(.*)',
+  '/account/login(.*)',
   '/monitoring',
-  '/account/login',
-  '/account/create',
 ]);
 
 export default clerkMiddleware((auth, req) => {
